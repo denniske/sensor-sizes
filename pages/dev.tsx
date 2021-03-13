5c49c9efbb076f47f0e8fdb3d4ec23c63aa916f2
Dennis Keil <dennis.keil10@gmail.com>
2021-03-13 11:40:44 +0100

    Changes

--- pages/dev.tsx
+++ pages/dev.tsx
@@ -1,19 +1,22 @@
 import SensorLayout from '../src/components/sensor-layout';
-import {getSensorsFromSheet} from '../src/helper/get-rows';
+import {getSensorsFromSheet, getTextsFromSheet} from '../src/helper/get-rows';
 
 
-export default function Home({sensors}) {
+export default function Home({sensors, texts}) {
     return (
-        <SensorLayout sensors={sensors} dev={true}/>
+        <SensorLayout sensors={sensors} texts={texts} dev={true}/>
     )
 }
 
 export async function getServerSideProps() {
     const sensors = await getSensorsFromSheet();
+    const texts = await getTextsFromSheet();
     console.log(sensors);
+    console.log(texts);
     return {
         props: {
             sensors,
+            texts,
         },
     };
 }
